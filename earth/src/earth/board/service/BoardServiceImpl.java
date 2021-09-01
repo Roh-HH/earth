62590cd93b913a6696c44a1430b08535bc87bfa1
Roh-HH <86752980+Roh-HH@users.noreply.github.com>
2021-09-01 17:45:03 +0900

    Update BoardServiceImpl.java

--- earth/src/earth/board/service/BoardServiceImpl.java
+++ earth/src/earth/board/service/BoardServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import earth.board.dao.BoardDAOImpl;
 import earth.board.dto.BoardDTO;
+import earth.board.dto.TodayDTO;
 
 // 구현
 @Service		// component-scan 이용해 자동으로 빈으로 등록시킬 것임 (객체생성)
@@ -20,51 +21,104 @@ public class BoardServiceImpl implements BoardService {
 
 	// 한페이지의 게시글 목록 가져오기 (List)
 	@Override
-	public Map<String, Object> getArticleList(String pageNum) throws SQLException {
+	public Map<String, Object> getArticleList(String pageNum, int code) throws SQLException {
 		
-		// ** 게시글 페이지 관련 정보 세팅 ** 
-		// 한페이지에 보여줄 게시글의 수 
-		int pageSize = 10; 
-		// 현재 페이지 번호  
-		if(pageNum == null){ // list.jsp 라고만 요청했을때, 즉 pageNum 파라미터 안넘어왔을때.
-			pageNum = "1";
+		Map<String, Object> result = null;
+		
+		if(code == 1) {
+			/*
+			// ** 게시글 페이지 관련 정보 세팅 ** 
+			// 한페이지에 보여줄 게시글의 수 
+			int pageSize = 10; 
+			// 현재 페이지 번호  
+			if(pageNum == null){ // list.jsp 라고만 요청했을때, 즉 pageNum 파라미터 안넘어왔을때.
+				pageNum = "1";
+			}
+			
+			// 현재 페이지에 보여줄 게시글 시작과 끝 등등 정보 세팅 
+			int currentPage = Integer.parseInt(pageNum); // 계산을 위해 현재페이지 숫자로 변환하여 저장 
+			int startRow = (currentPage - 1) * pageSize + 1; // 페이지 시작글 번호 
+			int endRow = currentPage * pageSize; // 페이지 마지막 글번호
+	
+			// 밖에서 사용가능하게 if문 시작 전에 미리 선언
+			List<BoardDTO> articleList = null;  	// 전체(검색된) 게시글들 담아줄 변수
+			int count = 0; 				// 전체(검색된) 글의 개수 
+			int number = 0; 			// 브라우저 화면에 뿌려줄 가상 글 번호  
+			
+			count = boardDAO.getArticleCount();   // DB에 저장되어있는 전체 글의 개수를 가져와 담기
+			System.out.println("count : " + count);
+			// 글이 하나라도 있으면 글들을 다시 가져오기 
+			if(count > 0){
+				articleList = boardDAO.getArticles(startRow, endRow); 
+			}	
+			number = count - (currentPage-1) * pageSize; 	// 게시판 목록에 뿌려줄 가상의 글 번호 
+			
+			// Controller 에게 전달해야 되는 데이터가 많으니, HashMap에 넘겨 줄 데이터를 저장한 후 한번에 전달하기
+			result = new HashMap<String, Object>();
+			result.put("pageSize", pageSize);
+			result.put("pageNum", pageNum);
+			result.put("currentPage", currentPage);
+			result.put("startRow", startRow);
+			result.put("endRow", endRow);
+			result.put("articleList", articleList);
+			result.put("count", count);
+			result.put("number", number);
+			*/
 		}
 		
-		// 현재 페이지에 보여줄 게시글 시작과 끝 등등 정보 세팅 
-		int currentPage = Integer.parseInt(pageNum); // 계산을 위해 현재페이지 숫자로 변환하여 저장 
-		int startRow = (currentPage - 1) * pageSize + 1; // 페이지 시작글 번호 
-		int endRow = currentPage * pageSize; // 페이지 마지막 글번호
+		// 오늘의 실천 게시판 게시글 목록 가져오기 - 노현호
+		if(code == 5) {
+			// ** 게시글 페이지 관련 정보 세팅 ** 
+			// 한페이지에 보여줄 게시글의 수 
+			int pageSize = 10; 
+			// 현재 페이지 번호  
+			if(pageNum == null){ // list.jsp 라고만 요청했을때, 즉 pageNum 파라미터 안넘어왔을때.
+				pageNum = "1";
+			}
+			
+			// 현재 페이지에 보여줄 게시글 시작과 끝 등등 정보 세팅 
+			int currentPage = Integer.parseInt(pageNum); 
+			int startRow = (currentPage - 1) * pageSize + 1; 		// 페이지 시작글 번호 
+			int endRow = currentPage * pageSize; 					// 페이지 마지막 글번호
 
-		// 밖에서 사용가능하게 if문 시작 전에 미리 선언
-		List<BoardDTO> articleList = null;  	// 전체(검색된) 게시글들 담아줄 변수
-		int count = 0; 				// 전체(검색된) 글의 개수 
-		int number = 0; 			// 브라우저 화면에 뿌려줄 가상 글 번호  
-		
-		count = boardDAO.getArticleCount();   // DB에 저장되어있는 전체 글의 개수를 가져와 담기
-		System.out.println("count : " + count);
-		// 글이 하나라도 있으면 글들을 다시 가져오기 
-		if(count > 0){
-			articleList = boardDAO.getArticles(startRow, endRow); 
-		}	
-		number = count - (currentPage-1) * pageSize; 	// 게시판 목록에 뿌려줄 가상의 글 번호 
-		
-		// Controller 에게 전달해야 되는 데이터가 많으니, HashMap에 넘겨 줄 데이터를 저장한 후 한번에 전달하기
-		Map<String, Object> result = new HashMap();
-		result.put("pageSize", pageSize);
-		result.put("pageNum", pageNum);
-		result.put("currentPage", currentPage);
-		result.put("startRow", startRow);
-		result.put("endRow", endRow);
-		result.put("articleList", articleList);
-		result.put("count", count);
-		result.put("number", number);
-		
+			// 밖에서 사용가능하게 if문 시작 전에 미리 선언
+			List<TodayDTO> articleList = null;  					// 전체(검색된) 게시글들 담아줄 변수
+			int count = 0; 											// 전체(검색된) 글의 개수 
+			int number = 0; 										// 브라우저 화면에 뿌려줄 가상 글 번호  
+			
+			// DB에 저장되어있는 전체 글의 개수를 가져와 담기
+			count = boardDAO.getArticleCount(code);					
+			System.out.println("전체 게시글 수 : " + count);
+			
+			// 글이 하나라도 있으면 글들을 다시 가져오기 
+			if(count > 0){
+				articleList = boardDAO.getArticles(startRow, endRow, code); 
+			}	
+			number = count - (currentPage-1) * pageSize; 	// 게시판 목록에 뿌려줄 가상의 글 번호 
+			
+			// Controller 에게 전달해야 되는 데이터가 많으니, HashMap에 넘겨 줄 데이터를 저장한 후 한번에 전달하기
+			result = new HashMap<String, Object>();
+			result.put("pageSize", pageSize);
+			result.put("pageNum", pageNum);
+			result.put("currentPage", currentPage);
+			result.put("startRow", startRow);
+			result.put("endRow", endRow);
+			result.put("articleList", articleList);
+			result.put("count", count);
+			result.put("number", number);
+		}
 		return result;
 	}
 
+	@Override
+	public int upload(TodayDTO dto) throws SQLException {
+		int result = boardDAO.upload(dto);
+		return result;
+	}
+	
 	// 검색 한 글 목록 가져오기 (list 검색)
 	@Override
-	public Map<String, Object> getArticleSearch(String pageNum, String sel, String search) throws SQLException {
+	public Map<String, Object> getArticleSearch(String pageNum, String sel, String search, int code) throws SQLException {
 		// ** 게시글 페이지 관련 정보 세팅 ** 
 		// 한페이지에 보여줄 게시글의 수 
 		int pageSize = 10; 
@@ -82,9 +136,11 @@ public class BoardServiceImpl implements BoardService {
 		List<BoardDTO> articleList = null;  	// 전체(검색된) 게시글들 담아줄 변수
 		int count = 0; 				// 전체(검색된) 글의 개수 
 		int number = 0; 			// 브라우저 화면에 뿌려줄 가상 글 번호  
-				
-		count = boardDAO.getSearchArticleCount(sel, search); // 검색된 글의 총 개수 가져오기 
+		
+		// 검색된 글의 총 개수 가져오기
+		count = boardDAO.getSearchArticleCount(sel, search);  
 		System.out.println("검색 count : " + count);
+		
 		// 검색한 글이 하나라도 있으면 검색한 글 가져오기 
 		if(count > 0){
 			articleList = boardDAO.getSearchArticles(startRow, endRow, sel, search);  
