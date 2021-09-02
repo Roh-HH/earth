e4530e9d30d6b2422f0f987cbc4417fbe5dbb8b7
Roh-HH <86752980+Roh-HH@users.noreply.github.com>
2021-09-02 17:47:38 +0900

    Update BoardDAO.java

--- earth/src/earth/board/dao/BoardDAO.java
+++ earth/src/earth/board/dao/BoardDAO.java
@@ -4,20 +4,32 @@ import java.sql.SQLException;
 import java.util.List;
 
 import earth.board.dto.BoardDTO;
+import earth.board.dto.NoticeDTO;
 import earth.board.dto.TodayDTO;
 
 public interface BoardDAO {
 
 	// CRUD
 	
-	// 전체 게시글 글 개수 가져오기 - 노현호
+	// 전체 게시글 글 개수 가져오기(모든 테이블 사용 가능) - 노현호
 	public int getArticleCount(int code) throws SQLException;
 	
-	// 해당 페이지마다 게시글 목록 가져오기 (startRow, endRow) - 노현호
-	public List<TodayDTO> getArticles(int startRow, int endRow, int code) throws SQLException;
+	// 공지사항 게시글 목록 가져오기 - 노현호
+	public List<NoticeDTO> getNoticeArticles(int startRow, int endRow, int code) throws SQLException;
+	// 공지사항 게시글 업로드 - 노현호
+	public int insertNotice(NoticeDTO dto) throws SQLException;
+	// 공지사항 게시글 조회 - 노현호
+	public NoticeDTO getNoticeArticle(int boardnum) throws SQLException;
+	// 공지사항 글 수정 처리
+	public int updateNoticeArticle(NoticeDTO dto) throws SQLException;
+	// 공지사항 글 수정 처리(이미지 수정O)
+	public int updateNoticeArticleImg(NoticeDTO dto) throws SQLException;
 	
+	// 오늘의실천 게시글 목록 가져오기 - 노현호
+	public List<TodayDTO> getTodayArticles(int startRow, int endRow, int code) throws SQLException;
 	// 오늘의 실천 업로드 - 노현호
 	public int upload(TodayDTO dto) throws SQLException;
+	
 	
 	// 검색 된 게시글 개수 가져오기
 	public int getSearchArticleCount(String sel, String search) throws SQLException;
@@ -28,8 +40,7 @@ public interface BoardDAO {
 	public void insertArticle(BoardDTO dto) throws SQLException;
 	// 게시글 1개 정보 가져오기
 	public BoardDTO getArticle(int num) throws SQLException;
-	// 조회수 1씩 올리기
-	public void readcountUp(int num) throws SQLException;
+
 	
 	// 게시글 수정 위해서 pw 맞는지 체크하기
 	public int pwCheck(BoardDTO dto) throws SQLException;
