6a5333214c2a285bff8f5791b73a040bbaa33970
Roh-HH <86752980+Roh-HH@users.noreply.github.com>
2021-09-02 17:46:52 +0900

    Update BoardService.java
    공지사항 관련 기능 추가됨

--- earth/src/earth/board/service/BoardService.java
+++ earth/src/earth/board/service/BoardService.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.Map;
 
 import earth.board.dto.BoardDTO;
+import earth.board.dto.NoticeDTO;
 import earth.board.dto.TodayDTO;
 
 // 선언
@@ -15,11 +16,22 @@ public interface BoardService {
 	public Map<String, Object> getArticleList(String pageNum, int code) throws SQLException;
 	// 게시판 검색한 list 페이지 목록 가져오기 (list 검색) - 노현호
 	public Map<String, Object> getArticleSearch(String pageNum, String sel, String search, int code) throws SQLException;
+	
+	// 공지사항 게시글 업로드 - 노현호
+	public int insertNotice(NoticeDTO dto) throws SQLException;
 	// 오늘의 실천 업로드 - 노현호
 	public int upload(TodayDTO dto) throws SQLException;
 	
-	// 게시판 글 1개 가져오기 (content)
+	// 게시판 글 1개 가져오기 (조회) - 노현호
 	public BoardDTO getArticle(int num) throws SQLException;
+	
+	// 공지사항 글 1개 가져오기 (조회) - 노현호
+	public NoticeDTO getNoticeArticle(int boardnum)	throws SQLException;
+	// 공지사항 글 수정 처리(이미지 수정X)
+	public int updateNoticeArticle(NoticeDTO dto) throws SQLException;
+	// 공지사항 글 수정 처리(이미지 수정O)
+	public int updateNoticeArticleImg(NoticeDTO dto) throws SQLException;
+	
 	// 게시글 등록 처리 (writePro)
 	public void insertArticle(BoardDTO dto) throws SQLException;
 	
