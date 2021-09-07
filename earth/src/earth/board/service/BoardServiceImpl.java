20c10c1a8220c8f8ca1d3a246c5f698066921bde
Roh-HH <86752980+Roh-HH@users.noreply.github.com>
2021-09-08 00:09:15 +0900

    Update BoardServiceImpl.java

--- earth/src/earth/board/service/BoardServiceImpl.java
+++ earth/src/earth/board/service/BoardServiceImpl.java
@@ -1,16 +1,26 @@
 package earth.board.service;
 
+import java.io.File;
 import java.sql.SQLException;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import earth.board.dao.BoardDAOImpl;
 import earth.board.dto.BoardDTO;
+import earth.board.dto.BracketsDTO;
+import earth.board.dto.FreeCommentDTO;
+import earth.board.dto.FreeDTO;
+import earth.board.dto.MonthDTO;
 import earth.board.dto.NoticeDTO;
 import earth.board.dto.TodayDTO;
 
@@ -26,6 +36,9 @@ public class BoardServiceImpl implements BoardService {
 		
 		// 한페이지에 보여줄 게시글의 수 
 		int pageSize = 10; 
+		if(code == 3 || code == 6 || code == 7 || code == 8) {
+			pageSize = 9;
+		}
 		// 현재 페이지 번호  
 		if(pageNum == null){ // pageNum 파라미터 안넘어왔을때.
 			pageNum = "1";
@@ -54,7 +67,10 @@ public class BoardServiceImpl implements BoardService {
 			}
 			break;
 		case 2:
-			
+			if(count > 0){
+				List<FreeDTO> articleList = boardDAO.getFreeArticles(startRow, endRow, code); 
+				result.put("articleList", articleList);
+			}
 			break;
 		case 3:
 			
@@ -101,6 +117,18 @@ public class BoardServiceImpl implements BoardService {
 	}
 	
 	@Override
+	public int insertFree(FreeDTO dto) throws SQLException {
+		int result = boardDAO.insertFree(dto);
+		return result;
+	}
+	
+	@Override
+	public int uploadFreeComment(FreeCommentDTO dto) throws SQLException {
+		int result = boardDAO.uploadFreeComment(dto);
+		return result;
+	}
+	
+	@Override
 	public int upload(TodayDTO dto) throws SQLException {
 		int result = boardDAO.upload(dto);
 		return result;
@@ -111,6 +139,24 @@ public class BoardServiceImpl implements BoardService {
 		// 조회수 1 증가시기키 및 해당 글 정보 가져오기
 		NoticeDTO article = boardDAO.getNoticeArticle(boardnum);
 		return article;
+	}
+	
+	@Override
+	public FreeDTO getFreeArticle(int boardnum) throws SQLException {
+		FreeDTO article = boardDAO.getFreeArticle(boardnum);
+		return article;
+	}
+	
+	@Override
+	public List<FreeCommentDTO> getFreeComment(int boardnum) throws SQLException {
+		List<FreeCommentDTO> comment = boardDAO.getFreeComment(boardnum);
+		return comment;
+	}
+	
+	@Override
+	public String getRef(int boardnum, int code) throws SQLException {
+		String ref = boardDAO.getRef(boardnum, code);
+		return ref;
 	}
 	
 	@Override
@@ -125,52 +171,18 @@ public class BoardServiceImpl implements BoardService {
 		return result;
 	}
 	
-	// 검색 한 글 목록 가져오기 (list 검색) -- 추후 삭제예정(노현호)
 	@Override
-	public Map<String, Object> getArticleSearch(String pageNum, String sel, String search, int code) throws SQLException {
-		// ** 게시글 페이지 관련 정보 세팅 ** 
-		// 한페이지에 보여줄 게시글의 수 
-		int pageSize = 10; 
-		// 현재 페이지 번호  
-		if(pageNum == null){ // list.jsp 라고만 요청했을때, 즉 pageNum 파라미터 안넘어왔을때.
-			pageNum = "1";
-		}
-				
-		// 현재 페이지에 보여줄 게시글 시작과 끝 등등 정보 세팅 
-		int currentPage = Integer.parseInt(pageNum); // 계산을 위해 현재페이지 숫자로 변환하여 저장 
-		int startRow = (currentPage - 1) * pageSize + 1; // 페이지 시작글 번호 
-		int endRow = currentPage * pageSize; // 페이지 마지막 글번호
-					
-		// 밖에서 사용가능하게 if문 시작 전에 미리 선언
-		List<BoardDTO> articleList = null;  	// 전체(검색된) 게시글들 담아줄 변수
-		int count = 0; 				// 전체(검색된) 글의 개수 
-		int number = 0; 			// 브라우저 화면에 뿌려줄 가상 글 번호  
-		
-		// 검색된 글의 총 개수 가져오기
-		count = boardDAO.getSearchArticleCount(sel, search);  
-		System.out.println("검색 count : " + count);
-		
-		// 검색한 글이 하나라도 있으면 검색한 글 가져오기 
-		if(count > 0){
-			articleList = boardDAO.getSearchArticles(startRow, endRow, sel, search);  
-		}
-				
-		number = count - (currentPage-1) * pageSize; 	// 게시판 목록에 뿌려줄 가상의 글 번호 
-				
-		// Controller 에게 전달해야 되는 데이터가 많으니, HashMap에 넘겨 줄 데이터를 저장한 후 한번에 전달하기
-		Map<String, Object> result = new HashMap();
-		result.put("pageSize", pageSize);
-		result.put("pageNum", pageNum);
-		result.put("currentPage", currentPage);
-		result.put("startRow", startRow);
-		result.put("endRow", endRow);
-		result.put("articleList", articleList);
-		result.put("count", count);
-		result.put("number", number);
-				
+	public int updateFreeArticle(FreeDTO dto) throws SQLException {
+		int result = boardDAO.updateFreeArticle(dto);
 		return result;
-		}
-
+	}
+	
+	@Override
+	public List<BracketsDTO> getBrackets() throws SQLException {
+		List<BracketsDTO> categList = boardDAO.getBrackets();
+		return categList;
+	}
+	
 	// 이달의 챌린지 글 등록 - 이다희 
 	@Override
 	public void insertChallenge(MonthDTO dto) throws SQLException {
@@ -196,9 +208,15 @@ public class BoardServiceImpl implements BoardService {
 	}
 	
 	@Override
-	public int pwCheck(int boardnum, int code, String pw) throws SQLException {
-		int pwCheck = boardDAO.pwCheck(boardnum, code, pw);
-		return pwCheck;
+	public String getPw(int boardnum, int code) throws SQLException {
+		String hashedPassword = boardDAO.getPw(boardnum, code);
+		return hashedPassword;
+	}
+	
+	@Override
+	public String getCtt(int boardnum, int code) throws SQLException {
+		String ctt = boardDAO.getCtt(boardnum, code);
+		return ctt;
 	}
 
 	@Override
@@ -206,4 +224,5 @@ public class BoardServiceImpl implements BoardService {
 		int result = boardDAO.deleteArticle(boardnum, code);
 		return result;
 	}
+
 }
