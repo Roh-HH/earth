0de4a8f985a6369d3c04429ad65c317ecdecb17b
Dahui <ddaahee@gmail.com>
2021-09-26 00:02:33 +0900

    EarthTask 스케줄러 수정 끝 !

--- earth/src/earth/board/task/EarthTask.java
+++ earth/src/earth/board/task/EarthTask.java
@@ -20,7 +20,6 @@ public class EarthTask {
 	public void monthlyChInsert() throws SQLException {
 	
 		System.out.println("Quartz 실행 , 이달의 챌린지 등록 / 매달 1일 00시에 실행  ");
-		 
 		MonthDTO dto = new MonthDTO();
 		dto.setSubject("[테스트] 9/18 쿼츠 등록 24시 15분 ");
 		dto.setId("admin");
@@ -31,9 +30,7 @@ public class EarthTask {
 		dto.setMaxcount(5);
 		dto.setCode(4);
 		dto.setEnddate("21/09/31");
-		
 		sqlSession.insert("board.insertChallenge", dto);
-		
 	}
 	
 	//매일 00시에 포인트 0 으로 업데이트
@@ -43,16 +40,14 @@ public class EarthTask {
 		sqlSession.update("user.updatePoint");
 	}
 	
-	
 	//@Scheduled(cron = "59 28 14 * * *") 
 	//@Scheduled(cron = "0 03 16 * * *")
 	//@Scheduled(cron = "0 9 16 * * *")
 	//@Scheduled(cron = "0 35 10 * * *")
-	//@Scheduled(cron = "0 50 23 * * *") 
+	//@Scheduled(cron = "0 0 0 * * *") 
 	public void test () {
-		System.out.println("=======> Quartz 실행 테스트 용  ");
+		System.out.println("=======> Quartz 실행 테스트 000 12시에 실행 !!! 성공 !!  ");
 		 
 	}
 	
-
 }
