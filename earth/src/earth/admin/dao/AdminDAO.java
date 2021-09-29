27e447287a7d991ea4c17a7e4319734dc8e5ab22
Roh-HH <86752980+Roh-HH@users.noreply.github.com>
2021-09-29 11:19:46 +0900

    Update AdminDAO.java
    보드디티오 경로수정

--- earth/src/earth/admin/dao/AdminDAO.java
+++ earth/src/earth/admin/dao/AdminDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 import earth.admin.dto.AdminBoardDTO;
 import earth.admin.dto.AdminCommentDTO;
 import earth.admin.dto.AdminQuestionDTO;
-import earth.admin.dto.NoticeDTO;
+import earth.board.dto.NoticeDTO;
 import earth.user.dto.ReportDTO;
 import earth.user.dto.UserDTO;
 
