a2e7735cd505818112790e32ac8a458d2dc411a3
sort2592 <86753025+sort2592@users.noreply.github.com>
2021-09-29 12:21:15 +0900

    Add files via upload

--- earth/src/earth/admin/service/AdminService.java
+++ earth/src/earth/admin/service/AdminService.java
@@ -3,23 +3,27 @@ package earth.admin.service;
 import java.sql.SQLException;
 import java.util.Map;
 
-import earth.user.dto.ReportDTO;
-
 public interface AdminService {
 
 	public Map<String, Object> getUserList(String pageNum) throws SQLException;
 	
 	public Map<String, Object> getUserSearch(String pageNum, String sel, String search) throws SQLException;
 	
+	public void deleteUser(String id) throws SQLException;
+	
 	public Map<String, Object> getBoardList(String pageNum) throws SQLException;
 	
 	public Map<String, Object> getBoardSearchList(String pageNum, String sel, String search) throws SQLException;
 	
-	public void deleteUser(String id) throws SQLException;
+	public void deleteFree(String num) throws SQLException;
+	
+	public void deleteDiary(String num) throws SQLException;
 	
 	public Map<String, Object> getCommentList(String pageNum) throws SQLException;
 	
 	public Map<String, Object> getCommentSearch(String pageNum, String sel, String search) throws SQLException;
+	
+	public void deleteComment(String num, String code) throws SQLException;
 	
 	public Map<String, Object> getQuestionList(String pageNum) throws SQLException;
 	
@@ -29,13 +33,5 @@ public interface AdminService {
 	
 	public Map<String, Object> getNoticeSearch(String pageNum, String sel, String search) throws SQLException;
 	
-	public int insertReport(ReportDTO report) throws SQLException;
 	
-	public ReportDTO getReport(int reportnum) throws SQLException;
-	
-	public Map<String, Object> getReportList(String pageNum, String process) throws SQLException;
-	
-	public Map<String, Object> getReportSearch(String pageNum, String process, String sel, String search) throws SQLException;
-	
-	public int processReport(String id, int reportnum, int punish) throws SQLException;
 }
--- earth/src/earth/admin/service/AdminServiceImpl.java
+++ earth/src/earth/admin/service/AdminServiceImpl.java
@@ -12,9 +12,10 @@ import earth.admin.dao.AdminDAOImpl;
 import earth.admin.dto.AdminBoardDTO;
 import earth.admin.dto.AdminCommentDTO;
 import earth.admin.dto.AdminQuestionDTO;
-import earth.board.dto.NoticeDTO;
-import earth.user.dto.ReportDTO;
+
 import earth.user.dto.UserDTO;
+
+import earth.board.dto.NoticeDTO;
 
 @Service
 public class AdminServiceImpl implements AdminService{
@@ -102,6 +103,14 @@ public class AdminServiceImpl implements AdminService{
 		}
 
 		@Override
+		public void deleteUser(String id) throws SQLException {
+			
+			adminDAO.deleteUser(id);
+			
+		}
+		
+		
+		@Override
 		public Map<String, Object> getBoardList(String pageNum) throws SQLException {
 			
 			int pageSize = 10; 
@@ -175,12 +184,20 @@ public class AdminServiceImpl implements AdminService{
 					
 			return result;
 			
+			
 		}
 		
 		@Override
-		public void deleteUser(String id) throws SQLException {
+		public void deleteFree(String num) throws SQLException {
 			
-			adminDAO.deleteUser(id);
+			adminDAO.deleteFree(num);
+			
+		}
+		
+		@Override
+		public void deleteDiary(String num) throws SQLException {
+			
+			adminDAO.deleteDiary(num);
 			
 		}
 
@@ -220,6 +237,14 @@ public class AdminServiceImpl implements AdminService{
 			return result;
 			
 		}
+
+		@Override
+		public void deleteComment(String num, String code) throws SQLException {
+			
+			adminDAO.deleteComment(num, code);
+			
+		}
+		
 
 		@Override
 		public Map<String, Object> getCommentSearch(String pageNum, String sel, String search) throws SQLException {
@@ -430,91 +455,8 @@ public class AdminServiceImpl implements AdminService{
 					
 			return result;
 		}
-
-	// 신고 기능
-	@Override
-	public int insertReport(ReportDTO report) throws SQLException {
-		int result = adminDAO.insertReport(report);
-		return result;
-	}
+		
+		
+		
 	
-	@Override
-	public ReportDTO getReport(int reportnum) throws SQLException {
-		ReportDTO report = adminDAO.getReport(reportnum);
-		return report;
-	}
-
-	@Override
-	public Map<String, Object> getReportList(String pageNum, String process) throws SQLException {
-
-		int pageSize = 10; 
-
-		int currentPage = Integer.parseInt(pageNum);  
-		int startRow = (currentPage - 1) * pageSize + 1; 
-		int endRow = currentPage * pageSize; 
-
-		List<ReportDTO> reportList = null;
-		int count = 0; 
-		int number = 0;  
-
-		count = adminDAO.getReportCount(process); 
-		if(count > 0){
-			reportList = adminDAO.getReport(process, startRow, endRow); 
-		}	
-		number = count - (currentPage-1) * pageSize; 
-
-		Map<String, Object> result = new HashMap<String, Object>();
-		result.put("pageSize", pageSize);
-		result.put("pageNum", pageNum);
-		result.put("currentPage", currentPage);
-		result.put("startRow", startRow);
-		result.put("endRow", endRow);
-		result.put("reportList", reportList);
-		result.put("count", count);
-		result.put("number", number);
-
-		return result;
-	}
-
-	@Override
-	public Map<String, Object> getReportSearch(String pageNum, String process, String sel, String search) throws SQLException {
-		int pageSize = 10; 
-
-		if(pageNum == null){
-			pageNum = "1";
-		}
-
-		int currentPage = Integer.parseInt(pageNum); 
-		int startRow = (currentPage - 1) * pageSize + 1; 
-		int endRow = currentPage * pageSize;
-
-		List<ReportDTO> reportList = null;
-		int count = 0; 
-		int number = 0;  
-
-		count = adminDAO.getSearchReportCount(process, sel, search);
-		if(count > 0){
-			reportList = adminDAO.getReportSearch(process, startRow, endRow, sel, search);  
-		}
-
-		number = count - (currentPage-1) * pageSize; 
-
-		Map<String, Object> result = new HashMap<String, Object>();
-		result.put("pageSize", pageSize);
-		result.put("pageNum", pageNum);
-		result.put("currentPage", currentPage);
-		result.put("startRow", startRow);
-		result.put("endRow", endRow);
-		result.put("reportList", reportList);
-		result.put("count", count);
-		result.put("number", number);
-
-		return result;
-	}
-
-	@Override
-	public int processReport(String id, int reportnum, int punish) throws SQLException {
-		int result = adminDAO.processReport(id, reportnum, punish);
-		return result;
-	}
 }
